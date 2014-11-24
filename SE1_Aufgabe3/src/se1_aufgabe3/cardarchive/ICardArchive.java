152ec9e076b8525dd7847daf3f084bc8fb45f912
TorbenHaug <claustorbenhaug@hotmail.de>
2014-11-24 20:00:04 +0100

    Some more comments

--- SE1_Aufgabe3/src/se1_aufgabe3/cardarchive/ICardArchive.java
+++ SE1_Aufgabe3/src/se1_aufgabe3/cardarchive/ICardArchive.java
@@ -11,19 +11,62 @@ import se1_aufgabe3.cardarchive.antwort.Antwort;
 import se1_aufgabe3.cardarchive.antwort.IAntwort;
 
 /**
- *
- * @author ClausTorben
+ * Zentrale Lernkartenverwaltungskomponente
+ * 
  */
 public interface ICardArchive {
+	/**
+	 * gibt ein Fach anhand seines Namens zurück
+	 * 
+	 * @param inName
+	 * @return IFach
+	 */
     IFach getFachByName(String inName);
+    
+    /**
+     * Gibt alle registrierten Fächer zurück
+     * 
+     * @return Collection<IFach>
+     */
     Collection<IFach> getFaecher();
+    
+    /**
+     * Erstellt und speichert ein Fach
+     * @param inName
+     * @return
+     */
     IFach createWithName(String inName);
     
+    /**
+     * Gibt eine Lernkarte anhand seiner UUID zurück
+     * 
+     * @param inID
+     * @return ILernkarte<? extends IAntwort>
+     */
     ILernkarte<? extends IAntwort> getLernkarteByID(UUID inID);
+    
+    /**
+     * Gibt alle Lernkarten zurück 
+     * 
+     * @return Collection<ILernkarte<? extends IAntwort>>
+     */
     Collection<ILernkarte<? extends IAntwort>> getLernkarten();
     
+    /**
+     * Erstellt eine neue Antwort
+     * 
+     * @param type
+     * @return IAntwort
+     */
     IAntwort createAntwort(Antwort.AntwortType type);
 
+    /**
+     * Erstellt aus einer Collection von Lernkarten CardSelection
+     *  
+     * @param inFach - Fach das dieser Auswahl zugeordnet werden soll
+     * @param inSelection - Liste von lernkarten die der Selection zugeordnet werden soll.
+     * @return ICardSelection
+     */
     ICardSelection createSelection(IFach inFach, Collection<ILernkarte<? extends IAntwort>> inSelection);
 
     <T extends IAntwort> T prepareAnswer(ILernkarte<T> inKarte, Object inValue);
