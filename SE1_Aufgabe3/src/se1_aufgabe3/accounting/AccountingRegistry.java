209590f1c16c411752b47862984a90bb795c4678
TorbenHaug <claustorbenhaug@hotmail.de>
2014-11-24 19:48:25 +0100

    Comment of IAnswerRegistry

--- SE1_Aufgabe3/src/se1_aufgabe3/accounting/AccountingRegistry.java
+++ SE1_Aufgabe3/src/se1_aufgabe3/accounting/AccountingRegistry.java
@@ -1,7 +1,11 @@
 package se1_aufgabe3.accounting;
 
 import se1_aufgabe3.common.EmailType;
-
+/**
+ * Zentrale Account Verwaltungs Komponente
+ * 
+ *
+ */
 public class AccountingRegistry implements IAccountingRegistry {
 
 	
@@ -9,27 +13,42 @@ public class AccountingRegistry implements IAccountingRegistry {
 		
 	}
 	
+	/* (non-Javadoc)
+	 * @see se1_aufgabe3.accounting.IAccountingRegistry#getStudentMitKennung(java.lang.String)
+	 */
 	@Override
 	public IStudent getStudentMitKennung(String inKennung) {
 		return new Student("Tim Hagemann", inKennung, 2170310, 3, new EmailType("tim.hagemann@haw-hamburg.de"));
 	}
 
+	/* (non-Javadoc)
+	 * @see se1_aufgabe3.accounting.IAccountingRegistry#getStudentMitMatrikelNr(int)
+	 */
 	@Override
 	public IStudent getStudentMitMatrikelNr(int inNummer) {
 		return new Student("Tim Hagemann", "abq264", inNummer, 3, new EmailType("tim.hagemann@haw-hamburg.de"));
 	}
 
+	/* (non-Javadoc)
+	 * @see se1_aufgabe3.accounting.IAccountingRegistry#getMitarbeiterMitPersonalNr(int)
+	 */
 	@Override
 	public IMitarbeiter getMitarbeiterMitPersonalNr(int inNr) {
 		return new Mitarbeiter("Fritz Wurst", inNr, new EmailType("fritz.wurst@haw-hamburg.de"));
 	}
 
+	/* (non-Javadoc)
+	 * @see se1_aufgabe3.accounting.IAccountingRegistry#getProfessorMitPersonalNr(int)
+	 */
 	@Override
 	public IProfessor getProfessorMitPersonalNr(int inNr) {
 
 		return new Professor("Hans Wurst", inNr, "hans.wurst", new EmailType("hans.wurst@haw-hamburg.de"));
 	}
 
+	/* (non-Javadoc)
+	 * @see se1_aufgabe3.accounting.IAccountingRegistry#loginAs(java.lang.String, java.lang.String)
+	 */
 	@Override
 	public IPerson loginAs(final String inUser, final String inPassword)
 	{
--- SE1_Aufgabe3/src/se1_aufgabe3/answers/IAnswerRegistry.java
+++ SE1_Aufgabe3/src/se1_aufgabe3/answers/IAnswerRegistry.java
@@ -5,14 +5,42 @@ import se1_aufgabe3.accounting.IMitarbeiter;
 import se1_aufgabe3.accounting.IStudent;
 import se1_aufgabe3.cardarchive.ILernkarte;
 import se1_aufgabe3.cardarchive.antwort.IAntwort;
-
+/**
+ *Antwort Verwaltungskomponente
+ *
+ */
 public interface IAnswerRegistry
 {
+	/**
+	 * Gibt alle Antworten zurück, die Einstudent jemals abgegeben hat
+	 * 
+	 * @param inStudent
+	 * @return
+	 */
 	public Collection<IAbgegebeneAntwort> getAntwortenVonStudent(IStudent inStudent);
 
+	/**
+	 * Gibt die nächste Abgegebene Antwort, die noch nicht durch das System oder einen 
+	 * Mitarbeiter geprüft wurde, zurück
+	 * 
+	 * @return
+	 */
 	public IAbgegebeneAntwort getNaechsteUngepruefteAntwort();
 
+	/**
+	 * Gibt eine Collection von Antworten zurück, die von einem bestimmten Mitarbeiter geprüft wurden
+	 * 
+	 * @param inMitarbeiter
+	 * @return
+	 */
 	public Collection<IAbgegebeneAntwort> getGepruefteAntwortenVon(IMitarbeiter inMitarbeiter);
 
+	/**
+	 * Speichert eine gegebene Antwort.
+	 * 
+	 * @param inStudent
+	 * @param inLernkarte
+	 * @param inAntwort
+	 */
 	public <T extends IAntwort> void saveAnswer(IStudent inStudent, ILernkarte<T> inLernkarte, IAntwort inAntwort);
 }
