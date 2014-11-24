b3ca8630c729e959f6ed3c6765ba016a64b257a0
TorbenHaug <claustorbenhaug@hotmail.de>
2014-11-24 19:35:38 +0100

    Comment IAccountingRegistry

--- SE1_Aufgabe3/src/se1_aufgabe3/accounting/IAccountingRegistry.java
+++ SE1_Aufgabe3/src/se1_aufgabe3/accounting/IAccountingRegistry.java
@@ -2,12 +2,44 @@ package se1_aufgabe3.accounting;
 
 public interface IAccountingRegistry
 {
+	/**
+	 * Gibt eine Studenten anhand seiner Kennung zurück.
+	 * 
+	 * @param inKennung
+	 * @return
+	 */
 	public IStudent getStudentMitKennung(String inKennung);
+	
+	/**
+	 * Gibt eine Studenten anhand seiner MatrikelNr zurück.
+	 * 
+	 * @param inNummer
+	 * @return
+	 */
 	public IStudent getStudentMitMatrikelNr(int inNummer);
-
+	
+	/**
+	 * Gibt einen Mitarbeiter anhand seiner PersonalNr zurück
+	 * 
+	 * @param inNr
+	 * @return
+	 */
 	public IMitarbeiter getMitarbeiterMitPersonalNr(int inNr);
-
+	
+	/**
+	 * Gibt einen Mitarbeiter anhand seiner PersonalNr. zurück
+	 * 
+	 * @param inNr
+	 * @return
+	 */
 	public IProfessor getProfessorMitPersonalNr(int inNr);
 
+	/**
+	 * Prüft die Logindaten und gibt den dazugehörigen User zurück 
+	 * @param inUser - Username
+	 * @param inPassword - Password
+	 * @return IPerson
+	 * @throws InvalidCredentialsException
+	 */
 	public IPerson loginAs(String inUser, String inPassword);
 }
